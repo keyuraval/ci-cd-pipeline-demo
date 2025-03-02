e8a30dd64c956395a3ae502fb48f17e083404be3
keyurraval <ravalkeyur054@gmail.com>
2025-03-02 14:17:18 +0530

    AWS Test Changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! This is our CI/CD pipeline demo.");
+  res.send("Hello, World! This is our CI/CD pipeline demo.with jenkins");
 });
 
 app.listen(PORT, () => {
